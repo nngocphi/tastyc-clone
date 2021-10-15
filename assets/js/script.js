f50c8fb1aabfd8c9abfbb5a5f6e8cdc5ee27f86b
Nguyễn Ngọc Phi <nngocphi910@gmail.com>
2021-10-15 14:18:10 +0700

    Add JavaScript to welcome slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Header
 const header = document.querySelector(".header__container");
 
 window.onscroll = function () {
@@ -15,6 +16,40 @@ function scrollFunction() {
 	}
 }
 
+// Welcome slider
+const dots = document.querySelectorAll(".welcome-slider__dot");
+const slides = document.querySelectorAll(".welcome__img");
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+	showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+	var i;
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].classList.remove("active");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].classList.add("active");
+}
+
+// Mobile app img
 const mobileAppImg = document.querySelector(".mobile-app__img");
 
 window.addEventListener("scroll", scrolling);
@@ -28,3 +63,33 @@ function scrolling() {
 		mobileAppImg.classList.remove("show");
 	}
 }
+
+const aboutStastistic = document.querySelector(".about__statistics");
+
+window.addEventListener("scroll", () => {
+	const stastisticPosition = aboutStastistic.getBoundingClientRect().top;
+	const screenPosition = window.innerHeight * (4 / 5);
+
+	if (stastisticPosition < screenPosition) {
+		const counters = document.querySelectorAll(".statistics__counter");
+		const speed = 10000;
+
+		counters.forEach((counter) => {
+			const updateCount = () => {
+				const target = +counter.getAttribute("data-target");
+				const count = +counter.innerText;
+
+				const increment = target / speed;
+
+				if (count < target) {
+					counter.innerText = Math.ceil(count + increment);
+					setTimeout(updateCount, 10);
+				} else {
+					count.innerText = target;
+				}
+			};
+
+			updateCount();
+		});
+	}
+});
